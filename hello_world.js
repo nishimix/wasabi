99ad19c0b9d0be108073c18520c54c2982200982
Mariko Nishimi <nishimi@google.com>
2020-10-29 13:39:53 +0900

    legacy properties commented out

--- hello_world.js
+++ hello_world.js
@@ -46,20 +46,20 @@ looker.plugins.visualizations.add({
   // Id and Label are legacy properties that no longer have any function besides documenting
   // what the visualization used to have. The properties are now set via the manifest
   // form within the admin/visualizations page of Looker
-  id: "hello_world",
-  label: "Hello World",
-  options: {
-    font_size: {
-      type: "string",
-      label: "Font Size",
-      values: [
-        {"Large": "large"},
-        {"Small": "small"}
-      ],
-      display: "radio",
-      default: "large"
-    }
-  },
+  // id: "hello_world",
+  // label: "Hello World",
+  // options: {
+  //   font_size: {
+  //     type: "string",
+  //     label: "Font Size",
+  //     values: [
+  //       {"Large": "large"},
+  //       {"Small": "small"}
+  //     ],
+  //     display: "radio",
+  //     default: "large"
+  //   }
+  // },
   // Set up the initial state of the visualization
   create: function(element, config) {
 
